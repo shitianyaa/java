06a7709be23e350ad2b2632ea92bd2530d46626e
shitianyaa <shitianyaa01@gmail.com>
2025-09-21 08:34:49 +0800

    Update Swufer.java

--- firstweek9/Swufer.java
+++ firstweek9/Swufer.java
@@ -7,10 +7,11 @@ public class Swufer extends Student {
 	public static void main(String[] args) {
 		Swufer sw = new Swufer();
 		sw.age=17;
-		sw.qq=251970624;
+		sw.qq=2519706243;
 		sw.sleep();
 		System.out.println(sw.age);
 		System.out.println(sw instanceof Student);
 	
 }
+
 }
